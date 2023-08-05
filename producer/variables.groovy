14888e4e145a168bb7d387f4c863d45ba491a35b
MohamedBenChamakh <mohamed.ben.chamakh98@gmail.com>
2023-08-05 17:26:38 +0100

    add variables

---
+++ producer/variables.groovy
@@ -0,0 +1,5 @@
+def        NEXUS_VERSION = "nexus3"
+def        NEXUS_PROTOCOL = "http"
+def        NEXUS_URL = "172.17.0.1:10680" 
+def        NEXUS_REPOSITORY = "maven-snapshots"
+def        NEXUS_CREDENTIAL_ID = "nexus-password"
